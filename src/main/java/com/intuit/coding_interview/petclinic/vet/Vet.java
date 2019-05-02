927a40cd123183dd4000fa46784968969b73a122
Doug Kretz <richard.kretz@tealium.com>
2019-05-02 10:57:04 -0700

    Implementation for Vet

--- src/main/java/com/intuit/coding_interview/petclinic/vet/Vet.java
+++ src/main/java/com/intuit/coding_interview/petclinic/vet/Vet.java
@@ -1,4 +1,29 @@
 package com.intuit.coding_interview.petclinic.vet;
 
+import com.intuit.coding_interview.petclinic.appointment.Appointment;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
 public class Vet {
+
+        @Id @GeneratedValue long id;
+
+        private String firstName;
+        private String lastName;
+
+        @OneToMany(mappedBy = "vet")
+        private List<Appointment> appointments;
+
+        private Vet(){} //JPA
+
+        public Vet(String firstName, String lastName) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.appointments = new ArrayList<>();
+        }
 }
