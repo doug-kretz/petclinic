11e7cb3b54a005b7ab179c6bdb3e4e6478d5f0c5
Doug Kretz <richard.kretz@tealium.com>
2019-05-02 11:04:27 -0700

    Add appointments to Pet

--- src/main/java/com/intuit/coding_interview/petclinic/pet/Pet.java
+++ src/main/java/com/intuit/coding_interview/petclinic/pet/Pet.java
@@ -1,11 +1,9 @@
 package com.intuit.coding_interview.petclinic.pet;
 
+import com.intuit.coding_interview.petclinic.appointment.Appointment;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
@@ -21,10 +19,14 @@ public class Pet {
     private String firstName;
     private String lastName;
 
+    @OneToMany(mappedBy = "pet")
+    private List<Appointment> appointments;
+
     private Pet(){} //JPA nonsense
 
     public Pet( String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.appointments = new ArrayList<>();
     }
 }
