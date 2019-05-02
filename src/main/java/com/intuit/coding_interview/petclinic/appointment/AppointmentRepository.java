1322706964f257e6e5af12532b9ffa823a1e1a5c
Doug Kretz <richard.kretz@tealium.com>
2019-05-02 11:10:26 -0700

    Create CRUDRepository for Vet and Appointment

---
+++ src/main/java/com/intuit/coding_interview/petclinic/appointment/AppointmentRepository.java
@@ -0,0 +1,6 @@
+package com.intuit.coding_interview.petclinic.appointment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppointmentRepository extends CrudRepository {
+}
---
+++ src/main/java/com/intuit/coding_interview/petclinic/vet/VetRepository.java
@@ -0,0 +1,6 @@
+package com.intuit.coding_interview.petclinic.vet;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository {
+}
